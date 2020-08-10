7e6f94ca8dfdad5f28f826acdea0c12fccfa5228
ladymey <ladymeyy@gmail.com>
2020-08-11 00:47:17 +0300

    Delete producer.py

--- producer.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaProducer
-import json
-
-def init():
-    producer = KafkaProducer(
-        bootstrap_servers='localhost:9092',
-        value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-    return producer
